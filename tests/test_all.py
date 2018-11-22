55855f5996b8ae823e62fbae455f3f09d52ecf22
virgesmith <a.p.smith@leeds.ac.uk>
2018-11-22 19:51:24 +0000

    add test for scotland hh #20

--- tests/test_all.py
+++ tests/test_all.py
@@ -17,12 +17,29 @@ class Test(TestCase):
     num_occ_dwellings = 4385
     num_communal = 42
     pop_communal = 188
-    pop_occupied = 7073
+    pop_occupied_lb = 7073
 
     # any problems and assert will fail
     microsynth.run()
 
-    self.assertTrue(Utils.check_hh(microsynth, num_occ_dwellings, num_dwellings, num_communal, pop_occupied, pop_communal))
+    self.assertTrue(Utils.check_hh(microsynth, num_occ_dwellings, num_dwellings, num_communal, pop_occupied_lb, pop_communal))
+
+  # City of London MSOA (one geog area)
+  def test_sc1(self):
+    region = "S12000023" # Orkney is least populated council area
+    resolution = "OA11"
+    cache = "./cache"
+    microsynth = hh_msynth.Household(region, resolution, cache)
+    num_dwellings = 10357
+    num_occ_dwellings = 9725
+    num_communal = 69
+    pop_communal = 198
+    pop_occupied_lb = 20506
+
+    # any problems and assert will fail
+    microsynth.run()
+
+    self.assertTrue(Utils.check_hh(microsynth, num_occ_dwellings, num_dwellings, num_communal, pop_occupied_lb, pop_communal, scotland=True))
 
   # def test_hrp1(self):
   #   region = "E09000001"
