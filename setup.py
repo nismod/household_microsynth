305992d80f3edd9d5fec2941402429d00f1da6f7
Benjamin Wilson <12176376+benjaminisaac0111@users.noreply.github.com>
2019-10-24 14:45:10 +0100

    Change to setup config to build from Leeds fork of HH_microsynth.

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ def readme():
     return f.read()
 
 setup(name='household_microsynth',
-  version='0.0.1',
-  description='Household microsynthesis',
-  long_description=readme(),
-  url='https://github.com/nismod/household_microsynth',
-  author='Andrew P Smith',
-  author_email='a.p.smith@leeds.ac.uk',
-  license='MIT',
-  packages=['household_microsynth'],
-  zip_safe=False,
-  install_requires=['distutils_pytest', 'humanleague', 'ukcensusapi'],
-  dependency_links=['git+git://github.com/virgesmith/humanleague.git#egg=humanleague',
-                    'git+git://github.com/virgesmith/UKCensusAPI.git#egg=ukcensusapi'],
-  test_suite='nose.collector',
-  tests_require=['nose'],
-  python_requires='>=3'
-  #scripts=['scripts/run_microsynth.py']
-)
+      version='0.0.1',
+      description='Household microsynthesis',
+      long_description=readme(),
+      url='https://github.com/nismod/household_microsynth',
+      author='Andrew P Smith',
+      author_email='a.p.smith@leeds.ac.uk',
+      license='MIT',
+      packages=['household_microsynth'],
+      zip_safe=False,
+      install_requires=['distutils_pytest', 'humanleague', 'ukcensusapi'],
+      dependency_links=['git+git://github.com/BenjaminIsaac0111/humanleague.git#egg=humanleague',
+                        'git+git://github.com/BenjaminIsaac0111/UKCensusAPI.git#egg=ukcensusapi'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      python_requires='>=3'
+      # scripts=['scripts/run_microsynth.py']
+      )
